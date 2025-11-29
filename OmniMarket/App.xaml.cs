16cb2fab4a58b1269f7eb6ae917d894e51adac49
Lschanhi <lschanhi@outlook.com>
2025-11-28 22:02:05 -0300

    Começo de Programação

---
+++ OmniMarket/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace OmniMarket
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.FromHex("#0E0E0E"),
+                BarTextColor = Color.FromHex("#D4AF37")
+            };
+        }
+    }
+}
---
+++ OmniMarket/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace OmniMarket
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
